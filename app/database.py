8522cc553817e78046a629b1ecaa5c908a8a3c6a
artem <artemgolovachov1256789@gmail.com>
2026-09-24 10:29:31 +0200

    make  basic db

---
+++ app/database.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("URL")
+
+engine = create_engine(DATABASE_URL)
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+
